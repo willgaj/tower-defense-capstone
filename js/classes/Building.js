a9c7546d748d925ee9bf6f4ede9d4bec463e9097
William Gajewski <william.gajewski@merkle.com>
2022-11-22 14:53:18 -0500

    turret range only shows when hovering over placement tiles

--- js/classes/Building.js
+++ js/classes/Building.js
@@ -21,11 +21,6 @@ class Building extends Sprite {
 
     draw() {
         super.draw();
-
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        c.fillStyle = 'rgba(0, 0, 255, 0.2)';
-        c.fill();
     }
 
     update() {
--- js/classes/PlacementTile.js
+++ js/classes/PlacementTile.js
@@ -19,6 +19,11 @@ class PlacementTile {
         if (mouse.x > this.position.x - this.size && mouse.x < this.position.x + this.size * 2 &&
             mouse.y > this.position.y - this.size && mouse.y < this.position.y + this.size * 2) {
             this.color = 'white';
+            c.beginPath();
+            const turretRadius = 150; //this value needs to match the fire-radius of the turrets
+            c.arc(this.position.x, this.position.y, turretRadius, 0, Math.PI * 2);
+            c.fillStyle = 'rgba(0, 0, 255, 0.1)';
+            c.fill();
         }
         else {
             this.color = 'rgba(255, 255, 255, .15)';
